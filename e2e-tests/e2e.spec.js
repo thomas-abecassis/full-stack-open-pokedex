35b2941ec074cf9a624747876cd823a846dfec70
Abecassis Thomas <stingerkiller34@gmail.com>
2024-05-30 11:40:07 +0200

    add front page can navigate to a particular pokemon e2e test

--- e2e-tests/e2e.spec.js
+++ e2e-tests/e2e.spec.js
@@ -6,4 +6,12 @@ describe('Pokedex', () => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
+
+  test('front page can navigate to a particular pokemon', async ({ page }) => {
+    await page.goto('')
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await page.getByText('ivysaur').click()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+  })
 })
+
